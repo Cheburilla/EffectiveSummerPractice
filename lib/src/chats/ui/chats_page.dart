dfa97e1cd0873749cd23044c051929f2f1cb7fca
Alexander Safronov <sanya.safron2003@gmail.com>
2023-07-03 18:55:19 +0600

    solid fix

--- lib/src/chats/ui/chats_page.dart
+++ lib/src/chats/ui/chats_page.dart
@@ -23,124 +23,22 @@ class ChatsPage extends StatelessWidget {
         leading: const Icon(Icons.menu),
         actions: [
           IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () => showSearch(
-                context: context,
-                delegate: SearchPage(
-                    searchLabel: 'Поиск',
-                    suggestion: const Center(
-                      child: Text('Чаты по имени и сообщению'),
+              icon: const Icon(Icons.search),
+              onPressed: () => showSearch(
+                    context: context,
+                    delegate: SearchPage(
+                      searchLabel: 'Поиск',
+                      suggestion: const Center(
+                        child: Text('Чаты по имени и сообщению'),
+                      ),
+                      failure: const Center(
+                        child: Text('Ничего не найдено :('),
+                      ),
+                      items: chats,
+                      filter: (chat) => [chat.userName, chat.lastMessage],
+                      builder: (chat) => tiles(chat),
                     ),
-                    failure: const Center(
-                      child: Text('Ничего не найдено :('),
-                    ),
-                    items: chats,
-                    filter: (chat) => [chat.userName, chat.lastMessage],
-                    builder: (chat) => ListTile(
-                          title: Padding(
-                              padding: const EdgeInsets.only(bottom: 2, top: 4),
-                              child: Row(
-                                  textBaseline: TextBaseline.alphabetic,
-                                  crossAxisAlignment:
-                                      CrossAxisAlignment.baseline,
-                                  children: [
-                                    Expanded(
-                                      child: Text(chat.userName,
-                                          style: const TextStyle(
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.bold),
-                                          maxLines: 1,
-                                          overflow: TextOverflow.ellipsis),
-                                    ),
-                                    SizedBox(
-                                      child: Align(
-                                        alignment: Alignment.centerRight,
-                                        child: Text(
-                                          (DateTime.now()
-                                                      .difference(chat.date!)
-                                                      .inDays <
-                                                  7)
-                                              ? DateFormat.E("ru")
-                                                  .format(chat.date!)
-                                              : (DateUtils.isSameDay(
-                                                      DateTime.now(),
-                                                      chat.date!))
-                                                  ? DateFormat.Hm("ru")
-                                                      .format(chat.date!)
-                                                  : DateFormat.MMMd("ru")
-                                                      .format(chat.date!),
-                                          style: const TextStyle(
-                                              fontSize: 18, color: Colors.grey),
-                                        ),
-                                      ),
-                                    )
-                                  ])),
-                          subtitle: Row(
-                            textBaseline: TextBaseline.alphabetic,
-                            crossAxisAlignment: CrossAxisAlignment.baseline,
-                            children: [
-                              Expanded(
-                                child: Text(chat.lastMessage!,
-                                    style: const TextStyle(fontSize: 18),
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis),
-                              ),
-                              chat.countUnreadMessages != 0
-                                  ? Center(
-                                      child: Container(
-                                          //padding: const EdgeInsets.all(2),
-                                          width: 26,
-                                          decoration: const BoxDecoration(
-                                            shape: BoxShape.circle,
-                                            color: Colors.white,
-                                          ),
-                                          child: Center(
-                                              child: Text(
-                                            chat.countUnreadMessages.toString(),
-                                            style: const TextStyle(
-                                                fontSize: 18,
-                                                color: Colors.black,
-                                                fontWeight: FontWeight.w500),
-                                          ))))
-                                  : const SizedBox.shrink(),
-                            ],
-                          ),
-                          leading: CircleAvatar(
-                            radius: 28,
-                            backgroundImage: chat.userAvatar != null
-                                ? AssetImage(chat.userAvatar!)
-                                : null,
-                            child: chat.userAvatar == null
-                                ? Container(
-                                    decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        gradient: LinearGradient(
-                                            begin: Alignment.topCenter,
-                                            end: Alignment.bottomCenter,
-                                            colors: [
-                                              Colors.white,
-                                              Color.fromARGB(
-                                                  255,
-                                                  Random().nextInt(128),
-                                                  Random().nextInt(128),
-                                                  Random().nextInt(128))
-                                            ])),
-                                    child: chat.userAvatar == null
-                                        ? Center(
-                                            child: Text(
-                                            chat.userName[0],
-                                            style: const TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 24,
-                                                fontWeight: FontWeight.w600),
-                                          ))
-                                        : null,
-                                  )
-                                : null,
-                          ),
-                          visualDensity: const VisualDensity(vertical: 4),
-                        ))),
-          )
+                  ))
         ],
         centerTitle: true,
       ),
@@ -149,103 +47,100 @@ class ChatsPage extends StatelessWidget {
           itemCount: chats.length,
           itemBuilder: (BuildContext context, int index) {
             final chat = chats[index];
-            return ListTile(
-              title: Padding(
-                  padding: const EdgeInsets.only(bottom: 2, top: 4),
-                  child: Row(
-                      textBaseline: TextBaseline.alphabetic,
-                      crossAxisAlignment: CrossAxisAlignment.baseline,
-                      children: [
-                        Expanded(
-                          child: Text(chat.userName,
-                              style: const TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.bold),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis),
-                        ),
-                        SizedBox(
-                          child: Align(
-                            alignment: Alignment.centerRight,
-                            child: Text(
-                              (DateTime.now().difference(chat.date!).inDays < 7)
-                                  ? DateFormat.E("ru").format(chat.date!)
-                                  : (DateUtils.isSameDay(
-                                          DateTime.now(), chat.date!))
-                                      ? DateFormat.Hm("ru").format(chat.date!)
-                                      : DateFormat.MMMd("ru")
-                                          .format(chat.date!),
-                              style: const TextStyle(
-                                  fontSize: 18, color: Colors.grey),
-                            ),
-                          ),
-                        )
-                      ])),
-              subtitle: Row(
-                textBaseline: TextBaseline.alphabetic,
-                crossAxisAlignment: CrossAxisAlignment.baseline,
-                children: [
-                  Expanded(
-                    child: Text(chat.lastMessage!,
-                        style: const TextStyle(fontSize: 18),
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis),
-                  ),
-                  chat.countUnreadMessages != 0
-                      ? Center(
-                          child: Container(
-                              //padding: const EdgeInsets.all(2),
-                              width: 26,
-                              decoration: const BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Colors.white,
-                              ),
-                              child: Center(
-                                  child: Text(
-                                chat.countUnreadMessages.toString(),
-                                style: const TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.w500),
-                              ))))
-                      : const SizedBox.shrink(),
-                ],
-              ),
-              leading: CircleAvatar(
-                radius: 28,
-                backgroundImage: chat.userAvatar != null
-                    ? AssetImage(chat.userAvatar!)
-                    : null,
-                child: chat.userAvatar == null
-                    ? Container(
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            gradient: LinearGradient(
-                                begin: Alignment.topCenter,
-                                end: Alignment.bottomCenter,
-                                colors: [
-                                  Colors.white,
-                                  Color.fromARGB(
-                                      255,
-                                      Random().nextInt(128),
-                                      Random().nextInt(128),
-                                      Random().nextInt(128))
-                                ])),
-                        child: chat.userAvatar == null
-                            ? Center(
-                                child: Text(
-                                chat.userName[0],
-                                style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.w600),
-                              ))
-                            : null,
-                      )
-                    : null,
-              ),
-              visualDensity: const VisualDensity(vertical: 4),
-            );
+            return tiles(chat);
           }),
+    );
+  }
+
+  Widget tiles(Chat chat) {
+    return ListTile(
+      title: Padding(
+          padding: const EdgeInsets.only(bottom: 2, top: 4),
+          child: Row(
+              textBaseline: TextBaseline.alphabetic,
+              crossAxisAlignment: CrossAxisAlignment.baseline,
+              children: [
+                Expanded(
+                  child: Text(chat.userName,
+                      style: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis),
+                ),
+                SizedBox(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      (DateTime.now().difference(chat.date!).inDays < 7)
+                          ? DateFormat.E("ru").format(chat.date!)
+                          : (DateUtils.isSameDay(DateTime.now(), chat.date!))
+                              ? DateFormat.Hm("ru").format(chat.date!)
+                              : DateFormat.MMMd("ru").format(chat.date!),
+                      style: const TextStyle(fontSize: 18, color: Colors.grey),
+                    ),
+                  ),
+                )
+              ])),
+      subtitle: Row(
+        textBaseline: TextBaseline.alphabetic,
+        crossAxisAlignment: CrossAxisAlignment.baseline,
+        children: [
+          Expanded(
+            child: Text(chat.lastMessage!,
+                style: const TextStyle(fontSize: 18),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis),
+          ),
+          chat.countUnreadMessages != 0
+              ? Center(
+                  child: Container(
+                      //padding: const EdgeInsets.all(2),
+                      width: 26,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                      ),
+                      child: Center(
+                          child: Text(
+                        chat.countUnreadMessages.toString(),
+                        style: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500),
+                      ))))
+              : const SizedBox.shrink(),
+        ],
+      ),
+      leading: CircleAvatar(
+        radius: 28,
+        backgroundImage:
+            chat.userAvatar != null ? AssetImage(chat.userAvatar!) : null,
+        child: chat.userAvatar == null
+            ? Container(
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.white,
+                          Color.fromARGB(255, Random().nextInt(128),
+                              Random().nextInt(128), Random().nextInt(128))
+                        ])),
+                child: chat.userAvatar == null
+                    ? Center(
+                        child: Text(
+                        chat.userName[0],
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w600),
+                      ))
+                    : null,
+              )
+            : null,
+      ),
+      visualDensity: const VisualDensity(vertical: 4),
     );
   }
 }
